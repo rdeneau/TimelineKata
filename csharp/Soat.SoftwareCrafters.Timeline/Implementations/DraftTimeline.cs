afc985cab4cb9537f07abd3833adac373ef621a1
Romain Deneau <rdeneau@yahoo.fr>
2018-12-02 17:11:15 +0100

    Solution simple

--- csharp/Soat.SoftwareCrafters.Timeline/Implementations/DraftTimeline.cs
+++ csharp/Soat.SoftwareCrafters.Timeline/Implementations/DraftTimeline.cs
@@ -7,22 +7,28 @@ namespace Soat.SoftwareCrafters.Timeline.Implementations
 {
     public class DraftTimeline : ITimeline
     {
-        public IList<Date> Items { get; }
+        private readonly SortedSet<Date> _items;
+
+        public IList<Date> Items => _items.Select(Date.Clone).ToList();
 
         public DraftTimeline(IList<Date> items)
         {
-            Items = items;
+            if (items == null) throw new ArgumentNullException(nameof(items));
+            if (items.Count < 2) throw new ArgumentException("At least 2 items expected", nameof(items));
+            if (!items.IsSorted()) throw new ArgumentException("Sorted items expected", nameof(items));
+            _items = new SortedSet<Date>(items.Select(Date.Clone));
         }
 
         public void Add(Date item)
         {
-            Items.Add(item);
+            if (item == null) throw new ArgumentNullException(nameof(item));
+            _items.Add(item);
         }
 
         public void Remove(Func<Date, bool> isItemToRemove)
         {
-            var itemsToRemove = Items.Where(isItemToRemove).ToList();
-            itemsToRemove.ForEach(item => Items.Remove(item));
+            var itemsToRemove = _items.Where(isItemToRemove).Take(_items.Count - 2).ToList();
+            itemsToRemove.ForEach(item => _items.Remove(item));
         }
     }
 }
